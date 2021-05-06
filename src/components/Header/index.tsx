055c055b7ceee9e1db6ca547dd0e1aa7ea0c1a92
Rodrigo Taveira de Melo <rodrigo.melo@axestecnologia.com.br>
2021-05-06 14:07:23 -0400

    update link

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,6 +1,6 @@
 import {format} from 'date-fns';
 import ptBR from 'date-fns/locale/pt-BR';
-
+import Link from 'next/link'
 import styles from  "./styles.module.scss"
 
 export function Header() {
@@ -10,7 +10,9 @@ export function Header() {
 
     return (
         <header className={styles.headerContainer}>
-            <img src="/logo.svg" alt="Podcastr" />
+            <Link href='/'>
+                <img src="/logo.svg" alt="Podcastr" />
+            </Link>
 
             <p>O melhor para você ouvir, sempre</p>
 
